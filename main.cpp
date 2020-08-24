f77657fd8c4bb917d0a33f908306387588a5e5c5
Marcello Salis <marcello.salis@sita.aero>
2020-08-24 20:44:56 +0200

    Download spreadsheets.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-//#include <iostream>
 #include <fstream>
 
 #include "data_provider.hpp"
@@ -9,16 +8,24 @@
 
 int main(int argc, char **argv)
 {
+  std::string url = "https://sheets.googleapis.com/v4/spreadsheets/1JJo8MzkpuhfvsaKVFVlOoNymscCt-Aw-1sob2IhpwXY/values:batchGet?ranges=combos!A2:R&key=AIzaSyDzQ0jCf3teHnUK17ubaLaV6rcWf9ZjG5E";
+
+  std::string file;
+
+  bool download;
+
   boost::program_options::options_description desc("options:");
 
   desc.add_options()
     ("help", "display help")
-    ("url", boost::program_options::value<std::string>(), "combo spreadsheets url");
+    ("url", boost::program_options::value<std::string>(), "combo spreadsheets url")
+    ("file", boost::program_options::value<std::string>(&file)->default_value("combos.json"), "combo spreadsheets url")
+    ("download", boost::program_options::value<bool>(&download)->default_value(true), "combo spreadsheets url");
 
   boost::program_options::variables_map args;
 
   boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), args);
-  boost::program_options::notify(args);    
+  boost::program_options::notify(args);
 
   if(args.count("help"))
   {
@@ -26,16 +33,18 @@ int main(int argc, char **argv)
     return 0;
   }
 
-  std::string url = "https://sheets.googleapis.com/v4/spreadsheets/1JJo8MzkpuhfvsaKVFVlOoNymscCt-Aw-1sob2IhpwXY/values:batchGet?ranges=combos!A2:R&key=AIzaSyDzQ0jCf3teHnUK17ubaLaV6rcWf9ZjG5E";
-
   if(args.count("url"))
     url = args["url"].as<std::string>();
-    std::fstream stream("combos.json");
 
-  DataProvider provider;
+  if(download)
+  {
+    std::fstream stream(file, std::fstream::out);
 
-  if(!provider.get(url_, stream))
-    return false;
+    DataProvider provider;
+
+    if(!provider.get(url, stream))
+      return -1;
+  }
 
   return 0;
 }
